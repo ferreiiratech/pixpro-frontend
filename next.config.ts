c3ae12564b77e1bceb26279ad79a2ffac689e17a
Leonardo Ferreira <ferreiiratech@gmail.com>
2025-10-15 21:37:50 -0300

    feat: adiciona configuração do Docker e arquivos necessários para o ambiente

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  experimental: {
+    optimizePackageImports: [
+      "lucide-react",
+      "@radix-ui/react-label",
+      "@radix-ui/react-separator",
+      "@radix-ui/react-slot",
+    ],
+  },
+  trailingSlash: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "prod.spline.design",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
